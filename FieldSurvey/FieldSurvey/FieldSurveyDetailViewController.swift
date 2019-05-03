4635c9a3f7a5578373cf5af92bb80dc2c9ca9a2d
Tyler Blood <twbcdc@mail.missouri.edu>
2019-05-03 14:48:59 -0500

    Image assets added

---
+++ FieldSurvey/FieldSurvey/FieldSurveyDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FieldSurveyDetailViewController.swift
+//  FieldSurvey
+//
+//  Created by Tyler Blood on 5/3/19.
+//  Copyright © 2019 Tyler Blood. All rights reserved.
+//
+
+import UIKit
+
+class FieldSurveyDetailViewController: UIViewController {
+    
+    //var fieldObservation: 
+
+    //@IBOutlet weak var iconImageView: UIImageView!
+    //@IBOutlet weak var titleLabel: UILabel!
+    //@IBOutlet weak var dateLabel: UILabel!
+    //@IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FieldSurvey/FieldSurvey/FieldSurveyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FieldSurveyViewController.swift
+//  FieldSurvey
+//
+//  Created by Tyler Blood on 5/3/19.
+//  Copyright © 2019 Tyler Blood. All rights reserved.
+//
+
+import UIKit
+
+class FieldSurveyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
